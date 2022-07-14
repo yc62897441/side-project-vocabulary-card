0de2fc9cb00ac9cc522dce93a417ff3fef353678
yc62897441 <yc62897441@gmail.com>
2022-07-14 14:32:51 +0800

    build signin post function

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -14,7 +18,10 @@ app.get('/', (req, res) => {
 app.get('/signin', (req, res) => {
   res.render('signin')
 })
-
+app.post('/signin', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
 
 app.listen(PORT, () => {
   console.log(`App is listening on http://localhost:${PORT}`)
