ab8d9abb360e3f165eb4909ad2258615b9cf123e
yc62897441 <yc62897441@gmail.com>
2022-07-16 13:17:26 +0800

    設置靜態檔案 static

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const PORT = 3000
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
