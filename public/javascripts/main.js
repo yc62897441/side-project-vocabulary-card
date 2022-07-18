d99626100e7004b166d27d997befb6f72f52a0de
yc62897441 <yc62897441@gmail.com>
2022-07-18 14:16:30 +0800

    中英切換、顯示的原件改用 querySelectorAll 選取全部

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 const vocabulary_containers = document.querySelector('.vocabulary_containers')
-const word_en = document.querySelector('.word_en')
-const word_ch = document.querySelector('.word_ch')
+const word_ens = document.querySelectorAll('.word_en')
+const word_chs = document.querySelectorAll('.word_ch')
 const btn_en_ch_shift = document.querySelector('.btn-en_ch_shift')
 const btn_show_shift = document.querySelector('.btn-show_shift')
 const btn_previous = document.querySelector('.btn-previous')
@@ -20,12 +20,12 @@ const model = {
 const controller = {
   en_ch_shift: function (event) {
     if (model.word_hidden === 'word_en') {
-      word_en.classList.remove('word_hidden')
-      word_ch.classList.add('word_hidden')
+      word_ens.forEach(word_en => word_en.classList.remove('word_hidden'))
+      word_chs.forEach(word_ch => word_ch.classList.add('word_hidden'))
       model.word_hidden = 'word_ch'
     } else if (model.word_hidden === 'word_ch') {
-      word_en.classList.add('word_hidden')
-      word_ch.classList.remove('word_hidden')
+      word_ens.forEach(word_en => word_en.classList.add('word_hidden'))
+      word_chs.forEach(word_ch => word_ch.classList.remove('word_hidden'))
       model.word_hidden = 'word_en'
     }
     model.word_hidden_on = false
@@ -34,16 +34,16 @@ const controller = {
     if (!model.word_hidden_on) {
       model.word_hidden_on = true
       if (model.word_hidden === 'word_en') {
-        word_en.classList.remove('word_hidden')
+        word_ens.forEach(word_en => word_en.classList.remove('word_hidden'))
       } else if (model.word_hidden === 'word_ch') {
-        word_ch.classList.remove('word_hidden')
+        word_chs.forEach(word_ch => word_ch.classList.remove('word_hidden'))
       }
     } else {
       model.word_hidden_on = false
       if (model.word_hidden === 'word_en') {
-        word_en.classList.add('word_hidden')
+        word_ens.forEach(word_en => word_en.classList.add('word_hidden'))
       } else if (model.word_hidden === 'word_ch') {
-        word_ch.classList.add('word_hidden')
+        word_chs.forEach(word_ch => word_ch.classList.add('word_hidden'))
       }
     }
   },
